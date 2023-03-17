fd8088b3b2eedcbfcadd05b2d9c1f0f13481f7ce
John Lenz <john.lenz27@gmail.com>
2023-03-17 16:44:59 -0700

    updated home view to see a list of machines and engineers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+        private object engineers;
+        private object machines;
+
+        public HomeController(FactoryContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      
+      List<object> model = new List< object>();
+      model.Add(engineers);
+      model.Add( machines);
+      return View(model);
+    }
+  }
 }
